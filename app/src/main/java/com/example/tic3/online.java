49126568d48bf05a5c60891509a8596bd53297b4
ad <aadarshnegi.2@gmail.com>
2021-12-06 00:33:19 +0530

    firebase issues

--- app/src/main/java/com/example/tic3/online.java
+++ app/src/main/java/com/example/tic3/online.java
@@ -9,9 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
@@ -39,140 +42,49 @@ public class online extends AppCompatActivity{
     DatabaseReference myRef = database.getReference("code");
     DatabaseReference pos = database.getReference("pos");
     static public int turn=0;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online);
         Create = findViewById(R.id.button4);
         Join = findViewById(R.id.button5);
-        code=findViewById(R.id.textView2);
-//            pos.setValue(124);
+        code = findViewById(R.id.textView2);
 
-//        mdb.child("code").setValue("it works");
-//        String cc = code.getText().toString();
     }
 
     public void create_code(View view) {
-        Create.setVisibility(View.INVISIBLE);
-        Join.setVisibility(View.INVISIBLE);
-        code.setVisibility(View.INVISIBLE);
         String cdx = code.getText().toString();
+        setContentView(R.layout.online_play);
+        myRef.setValue(cdx);
+        turn=1;
+        Toast.makeText(getApplicationContext(),"Connected",Toast.LENGTH_LONG).show();
 
-//        DataSnapshot snap
-
-        myRef.addValueEventListener(new ValueEventListener() {              // ref listenr
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                String val = dataSnapshot.getValue(String.class);
-//                val="heelop  ";
-//                Toast.makeText(getApplicationContext(),val,Toast.LENGTH_LONG).show();
-//                if(val.equals(cdx)){
-                myRef.setValue(cdx);
-//                    Intent intent = new Intent(online.this, online_play.class);
-//                    startActivity(intent);
-                    setContentView(R.layout.online_play);
-                    turn=1;
-                    Toast.makeText(getApplicationContext(),"connected",Toast.LENGTH_LONG).show();
-//                    online_play mm= new online_play();
-//                    mm.turn=1;
-
-//                }else Toast.makeText(getApplicationContext(),"invalid code",Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-//        ValueEventListener listen = new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for(DataSnapshot data: dataSnapshot.getChildren()){
-//                    if (data.child(cdx).exists()) {
-//                        mdb.child("code").setValue("it works");
-//                    } else {
-////                        DatabaseReference mdb;
-//                        mdb.child("code").setValue(cdx);
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        };
-//        mdb.child("codes").addValueEventListener(listen); {
-//            override fun onCancelled(error: DatabaseError) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                var check = isValueAvailable(snapshot, code)
-//
-//                Handler().postDelayed({
-//                if (check == true) {
-//                    Create.visibility = View.VISIBLE
-//                    Join.visibility = View.VISIBLE
-//                    GameCode.visibility = View.VISIBLE
-//                    textView4.visibility = View.VISIBLE
-//                    progressBar.visibility = View.GONE
-//
-//                } else {
-//                    FirebaseDatabase.getInstance().reference.child("codes").push().setValue(code)
-//                    isValueAvailable(snapshot, code)
-//                    checkTemp = false
-//                    Handler().postDelayed({
-//                            accepted()
-//                            errorMsg("Please don't go back")
-//                    }, 300)
-//
-//                }
-//                            }, 2000)
-//
-//
-//            }
-//
-//        })
-//}
 
     }
     static work_done ck = new work_done();
 
     public void make_move(View view) {
-//        res.getResourceEntryName(view.getId())
+            if(turn==1){
+                changC(view);
+                pos.setValue(view.getId());
+                turn=0;
+            }else{
+                Toast.makeText(getApplicationContext(),"wait for your turn",Toast.LENGTH_LONG).show();
+            }
 
-//        Toast.makeText(getApplicationContext(),a11,Toast.LENGTH_LONG).show();
-//        if(turn==1 || turn==0){
-//            turn=0;
-//            String loc = view.getId();
-//            Toast.makeText(getApplicationContext(),""+ view.getId(),Toast.LENGTH_LONG).show();
-//            int loc = view.getId();
-            changC(view);
-//            pos.setValue(view.getId());
+            pos.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    int val = snapshot.getValue(Integer.class);
+                    changC(findViewById(val));
+                    turn = 1;
+                }
 
-//        }else{
-//            turn = 1;
-//            Toast.makeText(getApplicationContext(),"" + view.getId(),Toast.LENGTH_LONG).show();
-//            pos.addValueEventListener(new ValueEventListener() {
-//                @Override
-//                public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                    int ppp = snapshot.getValue();
-//                    View vv = findViewById(ppp);
-//                    play.changC(vv);
-//                }
-//
-//                @Override
-//                public void onCancelled(@NonNull DatabaseError error) {
-//
-//                }
-//            });
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
 
-//        }
-
+                }
+            });
     }
 
 
@@ -184,8 +96,7 @@ public class online extends AppCompatActivity{
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String val = snapshot.getValue(String.class);
                 if(val.equals(cdx)){
-                    Intent intent = new Intent(online.this, MainActivity.class);
-                    startActivity(intent);
+                    setContentView(R.layout.online_play);
                     Toast.makeText(getApplicationContext(),"connected",Toast.LENGTH_LONG).show();
 
                 }else Toast.makeText(getApplicationContext(),"invalid code",Toast.LENGTH_LONG).show();
@@ -196,6 +107,7 @@ public class online extends AppCompatActivity{
 
             }
         });
+        make_move(null);
     }
     public void changC(View view) {
         TextView cur = findViewById(view.getId());
